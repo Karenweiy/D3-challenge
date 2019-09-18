c5d67376af5c563e121f6745e78946dfc7dbc469
Karen Wei <karenweiy@gmail.com>
2019-09-18 08:01:16 -0400

    first commit

---
+++ D3_data_journalism/assets/js/app.js
@@ -0,0 +1,106 @@
+// Step 1: Set up our chart
+//= ================================
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 70
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// // Step 3:
+// // Import data from the data.csv file
+// // =================================
+
+d3.csv("assets/data/data.csv", function(error, Data) {
+  if (error) throw error;
+
+  // Step 4: Parse the data
+  // Format the data and convert to numerical
+
+Data.forEach(function(record) {
+    record.poverty =+ record.poverty;
+    record.age =+ record.age;
+    record.income =+ record.income;
+    record.healthcare =+ record.healthcare;
+    record.obesity =+ record.obesity;
+    record.smokes =+ record.smokes;
+});
+
+// //Step 4: Create Scaling functions & axis functions for Healthcare vs. Poverty
+  var xScale = d3.scaleLinear()
+    .domain(d3.extent(Data, d => d.poverty))
+    .range([0, width]);
+
+  var yScale = d3.scaleLinear()
+    .domain([0, d3.max(Data, d => d.healthcare)])
+    .range([height, 0]);
+
+  var bottomAxis = d3.axisBottom(xScale);
+  var leftAxis = d3.axisLeft(yScale);
+
+// Append Axes to the chart
+
+  chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+  chartGroup.append("g")
+      .call(leftAxis);
+
+// //Step 5: Create Circles
+  
+   var circlesGroup = chartGroup.selectAll("circle")
+    .data(Data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xScale(d.poverty))
+    .attr("cy", d => yScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "skyblue")
+    .attr("opacity", ".5");
+
+// //Step 6: Insert Abbr. to circles
+    circlesGroup.append("text")
+     .attr("dy", "1.3em")
+     .style("text-anchor", "middle")
+     .data(function(d){
+      return d.abbr;
+     })
+     .enter()
+     .attr("font-size",10)
+     .attr("font-family",  "Gill Sans", "Gill Sans MT")
+     .attr("fill", "white");
+
+ // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("In poverty (%)");
+  });
